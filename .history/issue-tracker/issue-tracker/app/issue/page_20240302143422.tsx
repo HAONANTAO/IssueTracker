fdf44d02607cc5e501a56d08cf3f8d7786c3a87c
HAONANTAO <873190199@qq.com>
2024-03-02 14:34:31 +1100

    create IssuePage

---
+++ .history/issue-tracker/issue-tracker/app/issue/page_20240302143422.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const I = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default I
---
+++ .history/issue-tracker/issue-tracker/app/issue/page_20240302143426.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuePage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IssuePage
