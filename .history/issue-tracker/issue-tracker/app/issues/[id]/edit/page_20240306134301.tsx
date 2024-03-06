b9591a7cb686b62a747feb252bc228b28b9c692c
HAONANTAO <873190199@qq.com>
2024-03-06 21:46:10 +1100

    lazing loading

--- .history/issue-tracker/issue-tracker/app/issues/[id]/edit/page_20240306134301.tsx
+++ .history/issue-tracker/issue-tracker/app/issues/[id]/edit/page_20240306134301.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-
+import IssueForm from "@/app/components/IssueForm";
 import { Issue } from "@prisma/client";
 import { notFound } from "next/navigation";
-import dynamic from "next/dynamic";
+
 interface Props {
   params: { id: string };
 }
-
-const IssueForm = dynamic(() => import("@/app/components/IssueForm"), {
-  ssr: false,
-});
-
 const EditPage = async ({ params }: Props) => {
   const issue = await prisma?.issue.findUnique({
     where: { id: parseInt(params.id) },
--- .history/issue-tracker/issue-tracker/app/issues/[id]/edit/page_20240306214532.tsx
+++ .history/issue-tracker/issue-tracker/app/issues/[id]/edit/page_20240306214532.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import IssueForm from "@/app/components/IssueForm";
 import { Issue } from "@prisma/client";
 import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
@@ -7,9 +7,6 @@ interface Props {
   params: { id: string };
 }
 
-const IssueForm = dynamic(() => import("@/app/components/IssueForm"), {
-  ssr: false,
-});
 
 const EditPage = async ({ params }: Props) => {
   const issue = await prisma?.issue.findUnique({
