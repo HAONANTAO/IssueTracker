8fc914ffaecb7fab8752bc0e6612b0d836c63d9b
HAONANTAO <873190199@qq.com>
2024-03-07 20:19:29 +1100

    container for nav bar

---
+++ .history/issue-tracker/issue-tracker/app/Navbar_20240307201907.tsx
@@ -0,0 +1,54 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { FaBug } from "react-icons/fa6";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { Box, Container, Flex } from "@radix-ui/themes";
+const Navbar = () => {
+  const currentPath = usePathname();
+  const { status, data: session } = useSession();
+  // links labels
+  const link = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issue", href: "/issues" },
+  ];
+
+  return (
+    <>
+      <nav className="border-b space-x-4 px-2 py-2 mb-2 ">
+        <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <FaBug />
+            </Link>
+            <ul className="flex space-x-4 ">
+              {link.map((l) => (
+                <li key={l.label}>
+                  <Link
+                    href={l.href}
+                    className={`hover:text-zinc-600 transition-colors ${
+                      currentPath === l.href ? "text-zinc-800" : "text-zinc-400"
+                    }`}>
+                    {l.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Logout</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
