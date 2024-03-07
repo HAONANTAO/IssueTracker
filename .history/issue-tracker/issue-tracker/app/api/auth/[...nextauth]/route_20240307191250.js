e2279aea0c84340bba816d7d11d7fcebec0c372b
HAONANTAO <873190199@qq.com>
2024-03-07 19:26:03 +1100

    env save google client key

--- .history/issue-tracker/issue-tracker/app/api/auth/[...nextauth]/route_20240307191250.js
+++ .history/issue-tracker/issue-tracker/app/api/auth/[...nextauth]/route_20240307191250.js
@@ -1,12 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
+  providers,:[]
 });
 
 export { handler as GET, handler as POST };
--- .history/issue-tracker/issue-tracker/app/api/auth/[...nextauth]/route_20240307191256.js
+++ .history/issue-tracker/issue-tracker/app/api/auth/[...nextauth]/route_20240307191256.js
@@ -2,11 +2,10 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 const handler = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  })
 });
 
 export { handler as GET, handler as POST };
