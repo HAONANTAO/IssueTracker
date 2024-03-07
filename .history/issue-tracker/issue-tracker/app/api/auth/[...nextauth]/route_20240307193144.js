495cc31dc66b72fc56245f97c34dd862524ffab2
HAONANTAO <873190199@qq.com>
2024-03-07 19:35:20 +1100

    prisma save data

--- .history/issue-tracker/issue-tracker/app/api/auth/[...nextauth]/route_20240307193144.js
+++ .history/issue-tracker/issue-tracker/app/api/auth/[...nextauth]/route_20240307193144.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@nextauth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
